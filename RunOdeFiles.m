f1dbbe1e1b03f6833539ade4d15ba2c500f5ce19
amanchel <86847154+amanchel@users.noreply.github.com>
2021-08-03 15:28:55 -0400

    Add files via upload

--- RunOdeFiles.m
+++ RunOdeFiles.m
@@ -1,9 +1,21 @@
-% Script for running .ode model file using XPP-Matlab Interface
+% Matlab Script for running the .ode model file using XPP-Matlab Interface
 
-%clear all
+%clear all variables in the workspace
 clc
 
+% Edit the line below to point to the directory with the Model File
 cd('Path to Directory with ModelFile.ode')
+
+
+% Set the name of the odefile
 odefile = 'ModelFile.ode';
-out = RunXPP(odefile); % automatically names output file to 'output.dat'
-movefile('output.dat', 'RESULT.dat'); %rename output.dat file to RESULT.dat 
+
+
+% Run the simulation using the model file.
+% The output is automatically saved to a file named 'output.dat'
+%   in the same directory as the model file.
+out = RunXPP(odefile);
+
+
+% Moves the output file to a renamed file with the results
+movefile('output.dat', 'RESULT.dat');
